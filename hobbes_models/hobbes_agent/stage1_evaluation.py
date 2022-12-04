ce5253e640e3daea2611dcd44a6e9c22c1dc67fd
William Cutler <38445583+william-cutler@users.noreply.github.com>
2022-12-04 18:01:58 -0500

    Training configs

--- hobbes_models/hobbes_agent/stage1_evaluation.py
+++ hobbes_models/hobbes_agent/stage1_evaluation.py
@@ -13,10 +13,6 @@ import cv2
 from stage1_utils import get_episode_path, preprocess_image, save_gif
 
 
-# NOTE: Run from "hobbes_models/hobbes_agent/", "conda activate calvin_conda_env"
-HOBBES_DATASET_ROOT_PATH = "/home/grail/willaria_research/hobbes/dataset/"
-
-
 def initialize_env(robot_obs, scene_obs):
     with hydra.initialize(config_path="../../calvin_env/conf/"):
         env_config = hydra.compose(config_name="config_data_collection.yaml", overrides=["cameras=static_and_gripper"])
@@ -28,28 +24,27 @@ def initialize_env(robot_obs, scene_obs):
         env = hydra.utils.instantiate(env_config.env)
     env.reset(robot_obs, scene_obs)
     return env
-    
-def generate_trajectory(env, model, num_frames=20):
+
+
+@hydra.main(config_path="./conf/eval_config.yaml")
+def generate_trajectory(cfg, env, model):
     """Simulates the trajectory predicted by the model in the environment.
 
     Args:
-        model (_type_): Trained model to predict actions at each timestep.
-        env_config (_type_): Config for the environment
-        num_frames (int, optional): Number of steps to predict. Defaults to 20.
 
     Returns:
         _type_: _description_
     """
     frames = []
     actions = []
-    
-    for i in range(num_frames):
+
+    for i in range(cfg.num_trajectory_frames):
         # Get current frame
         curr_frame = env.render(mode="rgb_array")
-        
+
         # Generate action at current frame
         action = model(preprocess_image(curr_frame).unsqueeze(0))
-        
+
         # Save to lists
         frames.append(curr_frame)
         actions.append(action)
@@ -57,14 +52,15 @@ def generate_trajectory(env, model, num_frames=20):
         # Force gripper to binary -1 or 1
         action[0][6] = -1 if action[0][6] < 0 else 1
 
-        action_dict = {"type":"cartesian_rel", "action":action.detach().squeeze(0)}
+        action_dict = {"type": cfg.action_dict_key, "action": action.detach().squeeze(0)}
         # Step env
         observation, reward, done, info = env.step(action_dict)
-        
+
         if i % 10 == 0:
             print("Processed frame", i)
-    
+
     return frames, actions
+
 
 def display_frames(frames, title="", ms_per_frame=50):
     """Press any key to start video, once finished press a key to close. DO NOT HIT RED X!
@@ -79,29 +75,36 @@ def display_frames(frames, title="", ms_per_frame=50):
             _ = cv2.waitKey(0)
         cv2.imshow(title, frames[i][:, :, ::-1])
 
-        key = cv2.waitKey(50)  # pauses for 3 seconds before fetching next image
+        key = cv2.waitKey(ms_per_frame)
         if key == 27:  # if ESC is pressed, exit loop
             cv2.destroyAllWindows()
             break
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
+
+
 def main():
     # set up the start frame for the episode
-    ep = np.load(get_episode_path(360571, HOBBES_DATASET_ROOT_PATH + "/calvin_debug_dataset/training/"))
-    
+    ep = np.load(
+        get_episode_path(360571, "/home/grail/willaria_research/hobbes/dataset/calvin_debug_dataset/training/")
+    )
+
     # initialize env
     env = initialize_env(ep["robot_obs"], ep["scene_obs"])
-    
+
     # load model
-    #model = Stage1Model.load_from_checkpoint("./checkpoints/task_D_D/turn_on_lightbulb/latest-epoch=999.ckpt")
-    model = Stage1Model.load_from_checkpoint("./checkpoints/calvin_debug_dataset/turn_off_lightbulb/latest-epoch=999-v2.ckpt")
-    
+    # model = Stage1Model.load_from_checkpoint("./checkpoints/task_D_D/turn_on_lightbulb/latest-epoch=999.ckpt")
+    model = Stage1Model.load_from_checkpoint(
+        "./checkpoints/calvin_debug_dataset/turn_off_lightbulb/latest-epoch=999-v2.ckpt"
+    )
+
     # generate sample trajectory
-    frames, actions = generate_trajectory(env, model, num_frames=400)
+    frames, actions = generate_trajectory(env, model)
 
-    save_gif(frames, file_name='sample.gif')
-    # display_frames(frames)       
+    save_gif(frames, file_name="sample.gif")
+    # display_frames(frames)
 
+
+# NOTE: Run from "hobbes_models/hobbes_agent/", "conda activate calvin_conda_env"
 if __name__ == "__main__":
     main()
--- hobbes_models/hobbes_agent/stage1_training.py
+++ hobbes_models/hobbes_agent/stage1_training.py
@@ -14,10 +14,8 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from stage1_utils import get_episode_path, preprocess_image, save_gif
 
-# NOTE: Run from "hobbes_models/hobbes_agent/", "conda activate calvin_conda_env"
-HOBBES_DATASET_ROOT_PATH = "/home/grail/willaria_research/hobbes/dataset/"
 
-def collect_frames(start: int, stop: int, dataset_path: str, action_type: str = "rel_actions"):
+def collect_frames(start: int, stop: int, dataset_path: str, action_type: str):
     """Loads the specified range of episodes (semi-closed) for creating a demonstration video to imitate.
 
     Args:
@@ -33,9 +31,8 @@ def collect_frames(start: int, stop: int, dataset_path: str, action_type: str = 
         ep = np.load(get_episode_path(i, dataset_path))
         images.append(preprocess_image(ep["rgb_static"]))
         actions.append(torch.from_numpy(np.asarray(ep[action_type])).float())  # 'actions' or 'rel_actions'
-    save_gif(images, 'target.gif')
+    save_gif(images, "target.gif")
     return list(zip(images, actions))
-
 
 
 def get_task_timeframes(task_name: str, dataset_path: str, num_demonstrations: int) -> list:
@@ -55,36 +52,19 @@ def get_task_timeframes(task_name: str, dataset_path: str, num_demonstrations: i
     for (task, indx) in zip(lang["language"]["task"], lang["info"]["indx"]):
         if task == task_name:
             target_task_indxs.append(indx)
-        if (len(target_task_indxs) == num_demonstrations):
+        if len(target_task_indxs) == num_demonstrations:
             break
-    if (len(target_task_indxs) < num_demonstrations):
+    if len(target_task_indxs) < num_demonstrations:
         print(f"Warning: Requested {num_demonstrations} demonstrations but only found {len(target_task_indxs)}")
     return target_task_indxs
 
 
-def train_model(
-    task_name: str = "turn_on_led",
-    dataset_path: str = "calvin_debug_dataset",
-    model: Stage1Model = None,
-    model_save_path: str = "checkpoints/model_params",
-    num_demonstrations: int = 1,
-    val: bool = False,
-    batch_size: int = 16,
-    num_workers: int = 1,
-    num_gpus=0,
-    max_epochs=1000,
-    val_epochs=10
-) -> Stage1Model:
+@hydra.main(config_path="./conf/model_config.yaml")
+def train_model(cfg, model: Stage1Model = None) -> Stage1Model:
     """Train a model on a single task.
 
     Args:
-        task_name (str, optional): The name of the single task to train on. Defaults to 'turn_on_led'.
-        dataset_path (str, optional): Path to the desired dataset relative to HOBBES_DATASET_ROOT_PATH. Defaults to 'calvin_debug_dataset'.
-        model_save_path (str, optional): Path to store model checkpoints in. Defaul\ts to 'checkpoints/model_params'.
-        val (bool, optional): Whether to perform validation. Defaults to False.
-        batch_size (int, optional): Training batch size. Defaults to 16.
-        num_workers (int, optional): Number of workers for training dataloader. Defaults to 1.
-        num_gpus (int, optional): Number of GPU's to train on. Defaults to 0 (CPU).
+
     Returns:
         Stage1Model: The trained model.
     """
@@ -92,72 +72,55 @@ def train_model(
     if not model:
         model = Stage1Model()
 
-    train_data_path = HOBBES_DATASET_ROOT_PATH + dataset_path + "/training/"
-    train_timeframes = get_task_timeframes(task_name, train_data_path, num_demonstrations)
-    print(f"Found {len(train_timeframes)} demonstrations for the task {task_name}")
+    train_data_path = cfg.absolute_data_path + "/training/"
+    train_timeframes = get_task_timeframes(cfg.task_name, train_data_path, cfg.num_demonstrations)
+    print(f"Found {len(train_timeframes)} demonstrations for the task {cfg.task_name}")
     train_dataset = []
     for timeframe in train_timeframes:
-        train_dataset.extend(collect_frames(timeframe[0], timeframe[1], train_data_path))
+        train_dataset.extend(collect_frames(timeframe[0], timeframe[1], train_data_path, cfg.action_type))
     print("train dataset len", len(train_dataset))
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers)
+    train_dataloader = DataLoader(train_dataset, batch_size=cfg.batch_size, num_workers=cfg.num_workers)
 
-    val_dataloader = build_val_dataloader(task_name, dataset_path, batch_size, num_workers) if val else train_dataloader
-    monitor_str = "val_loss" if val else "train_loss"
+    val_dataloader = build_val_dataloader(val_timeframes=train_timeframes) if cfg.val else train_dataloader
+    monitor_str = "val_loss" if cfg.val else "train_loss"
 
-    checkpoint_callback = ModelCheckpoint(dirpath=model_save_path, save_top_k=3, monitor=monitor_str)
+    checkpoint_callback = ModelCheckpoint(dirpath=cfg.model_save_path, save_top_k=3, monitor=monitor_str)
     final_iter_callback = ModelCheckpoint(
-        dirpath=model_save_path, save_top_k=2, monitor="epoch", mode="max", filename="latest-{epoch:02d}"
+        dirpath=cfg.model_save_path, save_top_k=2, monitor="epoch", mode="max", filename="latest-{epoch:02d}"
     )
 
     trainer = pl.Trainer(
-        gpus=num_gpus,
+        gpus=cfg.num_gpus,
         num_nodes=1,
         precision=16,
         limit_train_batches=0.5,
         callbacks=[checkpoint_callback, final_iter_callback],
-        check_val_every_n_epoch=val_epochs,
-        max_epochs=max_epochs
+        check_val_every_n_epoch=cfg.epochs_between_val,
+        max_epochs=cfg.max_epochs,
     )
     trainer.fit(model, train_dataloader, val_dataloader)
 
     return model
 
 
-def build_val_dataloader(task_name: str, dataset_path: str, batch_size: int = 16, num_workers: int = 1) -> DataLoader:
-    """Builds the
-
-    Args:
-        task_name (str): _description_
-        dataset_path (str): _description_
-        batch_size (int, optional): _description_. Defaults to 16.
-        num_workers (int, optional): _description_. Defaults to 1.
-
-    Returns:
-        DataLoader: _description_
-    """
-    val_data_path = HOBBES_DATASET_ROOT_PATH + dataset_path + "/validation/"
-    val_timeframes = get_task_timeframes(task_name, val_data_path)
+@hydra.main(config_path="./conf/model_config.yaml")
+def build_val_dataloader(cfg, val_timeframes) -> DataLoader:
+    val_data_path = cfg.absolute_data_path + "/validation/"
     val_dataset = []
     for timeframe in val_timeframes:
-        val_dataset.extend(collect_frames(timeframe[0], timeframe[1], val_data_path))
+        val_dataset.extend(collect_frames(timeframe[0], timeframe[1], val_data_path, cfg.action_type))
     print("val dataset len", len(val_dataset))
-    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, num_workers=num_workers)
+    val_dataloader = DataLoader(val_dataset, batch_size=cfg.batch_size, num_workers=cfg.num_workers)
     return val_dataloader
 
+
 def main():
-    model = Stage1Model.load_from_checkpoint("./checkpoints/calvin_debug_dataset/turn_off_lightbulb/latest-epoch=999-v2.ckpt")
+    model = Stage1Model.load_from_checkpoint(
+        "./checkpoints/calvin_debug_dataset/turn_off_lightbulb/latest-epoch=999-v2.ckpt"
+    )
 
     # train model
-    train_model(
-        task_name="turn_off_lightbulb",
-        dataset_path="calvin_debug_dataset",
-        model=model,
-        model_save_path="checkpoints/calvin_debug_dataset/turn_off_lightbulb/long_train",
-        batch_size=16,
-        num_workers=1,
-        num_gpus=1,
-        max_epochs=10000,
-        val_epochs=10)
+    train_model(model=model)
 
     # # load model
     # ep = np.load(get_episode_path(360575, HOBBES_DATASET_ROOT_PATH + "/calvin_debug_dataset/training/"))
@@ -166,7 +129,9 @@ def main():
     # model = Stage1Model.load_from_checkpoint("./checkpoints/task_D_D/turn_on_lightbulb/latest-epoch=999.ckpt")
     # frames = build_frames(model, env_config, num_frames=100)
     # display_frames(frames)
-    # #360575
+    #
 
+
+# NOTE: Run from "hobbes_models/hobbes_agent/", "conda activate calvin_conda_env"
 if __name__ == "__main__":
     main()
